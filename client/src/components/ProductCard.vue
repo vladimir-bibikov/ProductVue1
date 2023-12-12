d5cb6aed5763935f611a3552dc1a3710cd1ac212
AlexRodving <alexrodving@gmail.com>
2023-12-12 21:30:18 +0300

    full

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="product-card">
+        <img src="JS2.jpg" alt="">
+        <h3>Title</h3>
+        <p>Description</p>
+        <p>2500</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: {},
+    },
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+.product-card{
+    width: 200px;
+    height: 300px;
+    box-shadow: 0 0 10px black;
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+
+    & img{
+        width: 100%;
+    }
+}
+</style>
--- client/src/views/BlogView.vue
+++ client/src/views/BlogView.vue
@@ -2,26 +2,43 @@
     <div>
         <h1>Blog</h1>
     </div>
-    <HW msg="Web33"></HW>
+    <div v-for="product in products" class="products" :key="product.id">
+        <ProductCard :product="product" />
+    </div>
+    
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+import ProductCard from '../components/ProductCard.vue';
 export default {
     components:{
-        HW: HelloWorld
+       ProductCard: ProductCard,
     },
     data () {
         return {
-
+            products: [],
         }
     },
     methods: {
 
     },
+    async beforeMount(){
+        const data = await fetch('http://localhost:1337/api/products?populate=*')
+        const js = await data.json()
+        this.products = js.data
+        console.log(this.products)
+    }
 }
 </script>
 
 <style scoped>
-
+.products{
+    width: 90%;
+    height: max-content;
+    margin: 20px auto 20px auto;
+    overflow-y: auto;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+}
 </style>
